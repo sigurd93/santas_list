54b5cf812b6de4d13f9710046ee5df9eb220041c
Sigurd Holme Ulvig <siguru17@student.uia.no>
2022-10-06 16:00:33 +0200

    Chose: examples.json

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 import { allowedNodeEnvironmentFlags } from "process";
+const db = require("../Backend/classes/database")
 
+dbinterface = db();
 const app = express();
 
 app.use(bodyParser.urlencoded({
@@ -11,3 +13,18 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"))
 
+app.get('/events', function (req, res) {
+    const result = dbinterface.GetByOrgName(req)
+    res.send(result)
+    })
+
+    app.get('/company', function (req, res) {
+    
+    })
+
+var server = app.listen(8080, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+    })
+    

9c2f83f77002111f8f7852ec064e9935df53e827
Haakon Mikaelsen <haakom17@uia.no>
2022-10-06 22:31:32 +0200

    base project structure

---
+++ Frontend/fif-santas-list/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+function App() {
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  return (
+    <div className="App">
+      <div>
+        <h1 className='maintitle'>FIF - Santas List</h1>
+      </div>
+      <p>Hallo!</p>
+      <p>Testing 1</p>
+      <p>Testing 2</p>
+    </div>
+  );
+  
+}
+
+
+
+
+export default App;
